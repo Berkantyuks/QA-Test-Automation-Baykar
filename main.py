ffc91666caebcefdebd4760bb93f9089d8ac0d00
Berkantyuks <byuksektepe@hotmail.com>
2022-06-16 17:45:33 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("There is a just main.py")
+    print("For run tests type these codes in terminal: ")
+    print("robot -d results  tests")
